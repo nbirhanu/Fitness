3cb0ea129b3d5757ac5a59056403acc2d3070766
nbirhanu <nbirhanu80@gmail.com>
2023-09-16 15:58:48 +0300

    fitness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import './App.css'
+import Navbar from './Components/Navbar'
+import Homepage from './pages/Homepage'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Footer from './Components/Footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Box>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/exercise/:id" element={<ExerciseDetail />} />
+				</Routes>
+				<Footer />
+			</Box>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExerciseDetail = () => {
+	return (
+		<div>
+			<h1>exercisedetail</h1>
+		</div>
+	)
+}
+
+export default ExerciseDetail
